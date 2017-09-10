0adc1dbb173c271903487bb0d52ef92364522bc2
Prokopev Alexandr <a.prokopev@qiwi.com>
2017-09-10 15:34:27 +0300

    new HandlerAttribute fields

--- FinBot.BotCore/src/Handlers/HandlerDescriptor.cs
+++ FinBot.BotCore/src/Handlers/HandlerDescriptor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using FinBot.BotCore.Commands;
 using FinBot.BotCore.Middlewares;
 using FinBot.BotCore.Utils;
@@ -8,28 +10,34 @@ namespace FinBot.BotCore.Handlers {
     public class HandlerDescriptor {
         public MethodInfo Method { get; }
 
-        private Maybe<string> Command { get; }
-        
-        private Maybe<string> Type { get; }
-
-        private HandlerDescriptor(MethodInfo method, string command, string type) {
+        private HandlerAttribute Attribute { get; }
+           
+        private HandlerDescriptor(MethodInfo method, HandlerAttribute attribute) {
             Method = method;
-            Command = command.Nullable();
-            Type = type.Nullable();
+            Attribute = attribute;
         }
         
         public HandlerMatch Match(MiddlewareData middlewareData) {
             var command = middlewareData.Features.RequireOne<CommandFeature>().Command;
 
-            var commandMatched = Command
+            var commandMatched = Attribute.Command.Nullable()
                 .Map(a => command.Command.Map(b => a.Equals(b, StringComparison.OrdinalIgnoreCase)).OrElse(false))
                 .OrElse(false);
+            
+            var commandsMatched = Attribute.Commands.Nullable()
+                .Map(a => command.Command.Map(b => a.Contains(b, StringComparer.OrdinalIgnoreCase)).OrElse(false))
+                .OrElse(false);
+            
+            var commandPatternMatched = Attribute.CommandPattern.Nullable()
+                .Map(pattern => new Regex(pattern, RegexOptions.Compiled))
+                .Map(pattern => command.Command.Map(pattern.IsMatch).OrElse(false))
+                .OrElse(false);
 
-            var typeMatched = Type
+            var typeMatched = Attribute.Type.Nullable()
                 .Map(filter => filter.Equals(command.Type, StringComparison.Ordinal))
                 .OrElse(true);
 
-            if (commandMatched && typeMatched) {
+            if (typeMatched && (commandMatched || commandsMatched || commandPatternMatched)) {
                 return HandlerMatch.CreateMatched(this);
             }
 
@@ -37,7 +45,7 @@ namespace FinBot.BotCore.Handlers {
         }
 
         public static HandlerDescriptor Create(MethodInfo method, HandlerAttribute attribute) {
-            return new HandlerDescriptor(method, attribute.Command, attribute.Type);
+            return new HandlerDescriptor(method, attribute);
         }
 
         public class HandlerMatch {
